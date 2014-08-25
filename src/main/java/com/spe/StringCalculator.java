550bf93088e45dd20876ddd2c849802c2384cf2a
Stephane Petit <hikaru95@gmail.com>
2014-08-25 18:45:09 +0200

    sonar issue

--- src/main/java/com/spe/StringCalculator.java
+++ src/main/java/com/spe/StringCalculator.java
@@ -8,68 +8,68 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-	private static final String CUSTOM_DELIMITER = "//(.)\n(.+)";
-	private static final String DEFAULT_DELIMITER = ",|\n";
+    private static final String CUSTOM_DELIMITER = "//(.)\n(.+)";
+    private static final String DEFAULT_DELIMITER = ",|\n";
 
-	private StringCalculator() {
-	}
+    private StringCalculator() {
+    }
 
-	public static int add(String text) {
+    public static int add(String text) {
 
-		if ("".equals(text)) {
-			return 0;
-		}
+        if ("".equals(text)) {
+            return 0;
+        }
 
-		String[] tokens = tokenize(text);
-		List<String> numbersToAdd = convertTokensToList(tokens);
+        String[] tokens = tokenize(text);
+        List<String> numbersToAdd = convertTokensToList(tokens);
 
-		checkForNegativeNumbers(numbersToAdd);
-		return convertAndSum(numbersToAdd);
-	}
+        checkForNegativeNumbers(numbersToAdd);
+        return convertAndSum(numbersToAdd);
+    }
 
-	private static List<String> convertTokensToList(String[] tokens) {
-		return Arrays.asList(tokens);
-	}
+    private static List<String> convertTokensToList(String[] tokens) {
+        return Arrays.asList(tokens);
+    }
 
-	private static String[] tokenize(String text) {
-		String delimiter = DEFAULT_DELIMITER;
-		String textToSum = text;
+    private static String[] tokenize(String text) {
+        String delimiter = DEFAULT_DELIMITER;
+        String textToSum = text;
 
-		Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
-		Matcher matcher = pattern.matcher(textToSum);
+        Pattern pattern = Pattern.compile(CUSTOM_DELIMITER);
+        Matcher matcher = pattern.matcher(textToSum);
 
-		if (matcher.find()) {
-			delimiter = matcher.group(1);
-			textToSum = matcher.group(2);
-		}
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            textToSum = matcher.group(2);
+        }
 
-		return textToSum.split(delimiter);
-	}
+        return textToSum.split(delimiter);
+    }
 
-	private static int convertAndSum(List<String> numbersToAdd) {
-		return numbersToAdd.stream().mapToInt(x -> convertStringToInt(x)).sum();
-	}
+    private static int convertAndSum(List<String> numbersToAdd) {
+        return numbersToAdd.stream().mapToInt(x -> convertStringToInt(x)).sum();
+    }
 
-	private static void checkForNegativeNumbers(List<String> numbersToAdd) {
-		List<String> negativeNumbers = filterNegativeNumbers(numbersToAdd);
+    private static void checkForNegativeNumbers(List<String> numbersToAdd) {
+        List<String> negativeNumbers = filterNegativeNumbers(numbersToAdd);
 
-		if (!negativeNumbers.isEmpty()) {
-			throw new NumberFormatException("negatives not allowed : "
-					+ negativeNumbers.toString());
-		}
-	}
+        if (!negativeNumbers.isEmpty()) {
+            throw new NumberFormatException("negatives not allowed : "
+                    + negativeNumbers.toString());
+        }
+    }
 
-	private static List<String> filterNegativeNumbers(List<String> numbersToAdd) {
-		return numbersToAdd.stream().filter(x -> convertStringToInt(x) < 0)
-				.collect(Collectors.toList());
-	}
+    private static List<String> filterNegativeNumbers(List<String> numbersToAdd) {
+        return numbersToAdd.stream().filter(x -> convertStringToInt(x) < 0)
+                .collect(Collectors.toList());
+    }
 
-	private static int convertStringToInt(String text) {
-		int result = Integer.parseInt(text);
-		if (result > 1000) {
-			result = 0;
-		}
-		return result;
-	}
+    private static int convertStringToInt(String text) {
+        int result = Integer.parseInt(text);
+        if (result > 1000) {
+            result = 0;
+        }
+        return result;
+    }
 
 }
